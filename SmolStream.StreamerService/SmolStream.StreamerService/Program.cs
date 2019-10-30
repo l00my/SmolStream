d82c7135cb1aae7ea3387b45f1eeaa21e228e20c
l00my <loomy91@gmail.com>
2019-10-30 19:56:30 +0000

    Added streamer service

---
+++ SmolStream.StreamerService/SmolStream.StreamerService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmolStream.StreamerService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
