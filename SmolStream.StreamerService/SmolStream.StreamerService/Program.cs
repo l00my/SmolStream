f7bb4ba1c869b3f639d987168b7ba5ffb45bf2b4
l00my <loomy91@gmail.com>
2019-10-30 23:04:07 +0000

    Added Game and Stream classes, added json.net to handle advanced deserialising, added static properties to remove repeated hardcoded values, added get games and streams methods, added parse response data method, wrapped console writing methods into helper classes, added code to get list of streamers with less than or equal to desired threshold

--- SmolStream.StreamerService/SmolStream.StreamerService/Program.cs
+++ SmolStream.StreamerService/SmolStream.StreamerService/Program.cs
@@ -1,10 +1,18 @@
-﻿using RestSharp;
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SmolStream.StreamerService
 {
     class Program
     {
+        private static readonly int _targetViewerCount = 150;
+        private static readonly int _firstValue = 100;
+        private static readonly string _twitchUrl = "https://api.twitch.tv/helix";
+        private static List<Game> _games = new List<Game>();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -18,16 +26,70 @@ namespace SmolStream.StreamerService
 
             Console.WriteLine("Valid ID entered, continuing...");
 
-            var twitchClient = new RestClient("https://api.twitch.tv/helix");
-            var request = new RestRequest("streams", Method.GET);
+            var twitchClient = new RestClient(_twitchUrl);
+
+            GetGames(clientId, twitchClient);
+
+            GetStreams(clientId, twitchClient);
+        }
+
+        private static void GetGames(string clientId, RestClient twitchClient)
+        {
+            var request = new RestRequest("games/top", Method.GET);
             request.AddHeader("Client-ID", clientId);
-            request.AddParameter("first", "100");
+            request.AddParameter("first", _firstValue);
 
             var response = twitchClient.Execute(request);
+            ParseResponseData(response, _games);
+            WaitForUser();
+        }
 
-            Console.WriteLine("Top 20 Streams:");
-            Console.WriteLine(response.Content);
+        private static void GetStreams(string clientId, RestClient twitchClient)
+        {
+            var request = new RestRequest("streams", Method.GET);
+            request.AddHeader("Client-ID", clientId);
+            request.AddParameter("first", _firstValue);
+            request.AddParameter("game_id", _games[0].ID);
+
+            var streams = new List<Stream>();
+            var response = twitchClient.Execute(request);
+            ParseResponseData(response, streams);
+
+            AddBreak();
+            Console.WriteLine($"Top 100 {_games[0].Name} Streamers:");
+            streams.ForEach(s => Console.WriteLine(s.ToString()));
+            WaitForUser();
+
+            AddBreak();
+            Console.WriteLine($"Top {_games[0].Name} Streamers with less than {_targetViewerCount} viewers:");
+            streams.Where(s => s.Viewers <= _targetViewerCount).ToList().ForEach(s => Console.WriteLine(s.ToString()));
+            WaitForUser();
+        }
+
+        private static void ParseResponseData<T>(IRestResponse response, IList<T> collection)
+        {
+            var parent = JObject.Parse(response.Content);
+            var data = parent.Value<JArray>("data");
+            foreach (var d in data)
+            {
+                collection.Add(d.ToObject<T>());
+            }
+
+            collection.ToList().ForEach(c => Console.WriteLine(c.ToString()));
+        }
+
+        private static void AddBreak()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void WaitForUser()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to continue");
             Console.ReadLine();
         }
+
     }
 }
---
+++ SmolStream.StreamerService/SmolStream.StreamerService/Stream.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SmolStream.StreamerService
+{
+    [DataContract]
+    public class Stream
+    {
+        [DataMember(Name = "id")]
+        public string ID { get; set; }
+        [DataMember(Name = "user_id")]
+        public string UserID { get; set; }
+        [DataMember(Name = "user_name")]
+        public string UserName { get; set; }
+        [DataMember(Name = "game_id")]
+        public int GameID { get; set; }
+        [DataMember(Name = "type")]
+        public string Type { get; set; }
+        [DataMember(Name = "title")]
+        public string Title { get; set; }
+        [DataMember(Name = "viewer_count")]
+        public int Viewers { get; set; }
+        [DataMember(Name = "started_at")]
+        public DateTime StartTime { get; set; }
+        [DataMember(Name = "language")]
+        public string Language { get; set; }
+        [DataMember(Name = "thumbnail_url")]
+        public string Thumbnail { get; set; }
+
+        public override string ToString()
+        {
+            return $"{UserName}, User ID: {UserID}, Game ID: {GameID}, Title: {Title}, Viewers: {Viewers}";
+        }
+    }
+}
