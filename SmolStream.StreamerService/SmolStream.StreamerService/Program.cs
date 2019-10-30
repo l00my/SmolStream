5951941c5fd75d3068b176b80ad10af0d42a7096
l00my <loomy91@gmail.com>
2019-10-30 20:21:37 +0000

    Added basic, temporary input for client id

--- SmolStream.StreamerService/SmolStream.StreamerService/Program.cs
+++ SmolStream.StreamerService/SmolStream.StreamerService/Program.cs
@@ -7,6 +7,16 @@ namespace SmolStream.StreamerService
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Twitch Client ID:");
+            var clientId = Console.ReadLine();
+            while (clientId.Length != 30)
+            {
+                Console.WriteLine("Invalid ID, please re-enter:");
+                clientId = Console.ReadLine();
+            }
+
+            Console.WriteLine("Valid ID entered, continuing...");
+            Console.ReadLine();
         }
     }
 }
